401e8705a5960435226f3e0cd5ef044992bdab47
yuanqing <desk@yuanqing.sg>
2020-04-07 20:07:38 +0800

    Add a timestamp

--- scripts/scrape.js
+++ scripts/scrape.js
@@ -2,15 +2,16 @@ const fs = require('fs-extra')
 const path = require('path')
 const fetchPluginsData = require('../src/fetch-plugins-data')
 
-const DATA_DIRECTORY = 'data'
-const META_DATA_FILE = 'index.json'
+const DATA_DIRECTORY_NAME = 'data'
+const META_DATA_FILE_NAME = 'index.json'
 
 async function main () {
-  const date = new Date().toISOString().slice(0, 10)
+  const date = new Date().toISOString()
   const pluginsData = await fetchPluginsData()
-  await writeFile({ date, plugins: pluginsData }, META_DATA_FILE)
+  await writeFile({ date, plugins: pluginsData }, META_DATA_FILE_NAME)
   const stats = extractStats(pluginsData)
-  await writeFile(stats, `${date}.json`)
+  const statsFileName = `${date.slice(0, 10)}.json`
+  await writeFile({ date, stats }, statsFileName)
 }
 main()
 
@@ -23,6 +24,6 @@ function extractStats (pluginsData) {
 }
 
 async function writeFile (data, fileName) {
-  const file = path.join(DATA_DIRECTORY, fileName)
+  const file = path.join(DATA_DIRECTORY_NAME, fileName)
   await fs.outputFile(file, `${JSON.stringify(data)}\n`, 'utf8')
 }
