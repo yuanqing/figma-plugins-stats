2eedf1f71517e01cee9ee9106a1ab18622bb15ad
yuanqing <desk@yuanqing.sg>
2020-04-11 22:40:10 +0800

    Rename

--- src/fetch-author-id.js
+++ src/fetch-author-id.js
@@ -1,15 +1,15 @@
 const fetch = require('./utilities/fetch')
 
-async function fetchAuthorId (authorHandle) {
+async function fetchAuthorId (profileHandle) {
   const response = await fetch(
-    `https://www.figma.com/api/profile_handle/${authorHandle}`
+    `https://www.figma.com/api/profile_handle/${profileHandle}`
   )
   const json = await response.json()
   if (json.error === true) {
     throw new Error('Invalid user handle')
   }
   if (typeof json.meta.team_profile !== 'undefined') {
-    throw new Error(`Need a user handle; \`${authorHandle}\` is a team handle`)
+    throw new Error(`Need a user handle; \`${profileHandle}\` is a team handle`)
   }
   return json.meta.user_profile.id
 }
